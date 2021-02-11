f69d2dd66f8ef811b6bc45f530f341b703c30513
H8rsbeware <57707422+H8rsbeware@users.noreply.github.com>
2021-02-11 15:31:33 +0000

    Sound, submission, syntax, and styling update
    -Nav items, on the HTP, LTA, and Play screens, spacing increased
    -Credits updated to include p5 and bfxr
    -Sound effects were added to the game
    -Mute button added
    -WIP page removed
    -Correction to the play page (said NnC/HTP > NnC/Play in the nav bar)
    -Documentation updated

--- Javascript/game.js
+++ Javascript/game.js
@@ -1,44 +1,29 @@
-function init(player, Op) {
+function init(player, Op, muted) {
     //Creating and defining the canvas 
     const canvas = document.getElementById("cvs");
     const ctx = canvas.getContext("2d");
     //Defining the board list
     let board = [];
+    //all possible wins
+    const winPos = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]];
     //Defining the amount of spaces
     const col = 3;
     const row = 3;
-    //Defining the size of squares (as an interval between lines)
     const SI = 150;
-    // Adding image sources
-    const Xtile = new Image();
-    Xtile.src = "Images/Xtile150.png";
-    const Otile = new Image();
-    Otile.src = "Images/Otile150.png";
-    //all possible wins
-    const winPos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
     //Game continues 
     let GO = false;
     let GD = new Array(9); //game data 
-    let CPlayer = player.human; //set the current player (tbc)
+    let CPlayer = player.human; //set the current player
 
-    function drawBoard() {
+    function drawBoard() { //Draws board in canvas
         let pos = 0;
-        for (let i = 0; i < row; i++) {
-            board[i] = [];
-            for (let j = 0; j < col; j++) {
-                board[i][j] = pos;
-                ctx.strokeStyle = "#FFF";
-                ctx.strokeRect(j * SI, i * SI, SI, SI);
-                pos++;
+        for (let i = 0; i < row; i++) { //For each row
+            board[i] = [];  //That position is set to empty
+            for (let j = 0; j < col; j++) { //For each colomn
+                board[i][j] = pos; //board coords are set to position
+                ctx.strokeStyle = "#FFF"; 
+                ctx.strokeRect(j * SI, i * SI, SI, SI); //Its drawn
+                pos++; //Position is increased
             }
         }
     }
@@ -64,28 +49,31 @@ function init(player, Op) {
         //Add move to canvas 
         drawEdit(CPlayer, i, j);
         //Endgame checks
-        if (isWin(GD, CPlayer)) {
-            showGO(CPlayer);
-            GO = true;
+        if (isWin(GD, CPlayer)) { //If the called function returns true
+            showGO(CPlayer); //End screen function is called with the winner
+            GO = true; //The game is ended
             return;
         }
-        if (isTie(GD)) {
+        if (isTie(GD)) {//Same basic premise but for a tie
             showGO("tie");
             GO = true;
             return;
         }
-        if (Op == "computer") {
-            let pos = minimax(GD, player.computer).pos;
+        //All of the above is for the main player
+
+        //All of the below is for the second player
+        if (Op == "computer") { //Checks if the player asks for algorithm
+            let pos = minimax(GD, player.computer).pos; //If it is, then minimax is initated
 
             GD[pos] = player.computer;
             console.log(GD);
 
-            let clickSpace = getClicked(pos);
+            let clickSpace = getClicked(pos); //Gets the computers clicked space
 
-            drawEdit(player.computer, clickSpace.clickI, clickSpace.clickJ);
+            drawEdit(player.computer, clickSpace.clickI, clickSpace.clickJ); //Draws the minimax returned move
 
-            if (isWin(GD, player.computer)) {
-                showGO(player.computer);
+            if (isWin(GD, player.computer)) { //Win and tie are same as main player
+                showGO(player.computer); //Feed the computer tile instead of the main players
                 GO = true;
                 return;
             }
@@ -94,55 +82,48 @@ function init(player, Op) {
                 GO = true;
                 return;
             } 
-        }else {
+        }else { //If not AI, the player is simply changed to the opposite human, who plays the same at the main player
             CPlayer = CPlayer == player.human ? player.friend : player.human;
         }
         
     });
-    //minimax
+    //minimax !RECURSION
     function minimax(GD, User) {
-        if (isWin(GD, player.computer)) return {
-            try: +10
-        };
-        if (isWin(GD, player.human)) return {
-            try: -10
-        };
-        if (isTie(GD)) return {
-            try: 0
-        };
+        if (isWin(GD, player.computer)) return {try: +100}; //Checks the game data returned last run for wins or ties
+        if (isWin(GD, player.human)) return {try: -100};
+        if (isTie(GD)) return {try: 0};
 
-        let ES = getSpace(GD);
-        let avMove = [];
+        let ES = getSpace(GD); //Gets all empty spaces after last runs game data 
+        let avMove = []; 
 
         for (let i = 0; i < ES.length; i++) {
-            let pos = ES[i];
-            let store = GD[pos];
-            GD[pos] = User
+            let pos = ES[i]; 
+            let store = GD[pos]; //stores the game data with the current position
+            GD[pos] = User //sets the new game data to "Current" player being tested (starts with computer)
 
-            let rMove = {};
-            rMove.pos = pos
+            let rMove = {}; //Stores the recent move
+            rMove.pos = pos //Sets the recent moves position to the current position
 
-            if (User == player.computer) {
+            if (User == player.computer) { //If the user is a computer, it runs the algorithm for the humans best moves
                 rMove.try = minimax(GD, player.human).try;
-            } else {
+            } else { //If the last test was for human moves, it instead tries for computer moves
                 rMove.try = minimax(GD, player.computer).try;
             }
-
-            GD[pos] = store;
-            avMove.push(rMove);
+            GD[pos] = store; //Resets the game data
+            avMove.push(rMove); //Stores the value of that move tried
         }
 
-        let bMove;
-        if (User == player.computer) {
-            bTry = -Infinity;
-            for (let i = 0; i < avMove.length; i++) {
-                if (avMove[i].try > bTry) {
-                    bTry = avMove[i].try;
+        let bMove; //Stores the best move
+        if (User == player.computer) { 
+            bTry = -Infinity; //The worse possible move at first, so anything is better
+            for (let i = 0; i < avMove.length; i++) { //For every tried move
+                if (avMove[i].try > bTry) { //If that move is "better" than the current best, its made the best
+                    bTry = avMove[i].try;   //Its also becomes the best try, that all others will be compared to
                     bMove = avMove[i];
                 }
             }
         } else {
-            bTry = +Infinity;
+            bTry = +Infinity; //Same idea, but for the humans moves so the number is the worse possible move the human can make
             for (let i = 0; i < avMove.length; i++) {
                 if (avMove[i].try < bTry) {
                     bTry = avMove[i].try;
@@ -152,64 +133,67 @@ function init(player, Op) {
         }
         return bMove;
     }
-    function getSpace(GD) {
+    function getSpace(GD) { 
         let clear = []
 
-        for (let pos = 0; pos < GD.length; pos++) {
-            if (!GD[pos]) clear.push(pos);
-        }
-        return clear
+        for (let pos = 0; pos < GD.length; pos++) { //For every space
+            if (!GD[pos]) clear.push(pos); //Its checked if it has any data, if not its added to the list
+        }return clear
     }
 
     function getClicked(pos) {
-        for(let i = 0; i < board.length; i++){
-            for(let j = 0; j < board[i].length; j++) {
-                if(board[i][j] == pos) return{clickI : i, clickJ : j}
+        for(let i = 0; i < board.length; i++){ 
+            for(let j = 0; j < board[i].length; j++) { 
+                if(board[i][j] == pos) return{clickI : i, clickJ : j} //Returns the clicked position to an object
             }
         }
     }
         //checks for wins
     function isWin(GD, player) {
-        for (let i = 0; i < winPos.length; i++) {
+        for (let i = 0; i < winPos.length; i++) { //for each 2D element in the 3D array (ie [0, 1, 2], [3, 4 ,5], [6, 7, 8])
             let won = true;
-            for (let j = 0; j < winPos[i].length; j++) {
+            for (let j = 0; j < winPos[i].length; j++) { //for each item in the 3D elements of the array (ie 0, 1, 2)
                 let pos = winPos[i][j];
-                won = GD[pos] == player && won;
-            }
-            if (won) {
-                return true;
-            }
-        }
-        return false;
+                won = GD[pos] == player && won; //If there are 3 in a row, it sets win to that specific position
+            }if (won) return true; //If won = true, then it returns true
+        }return false; // otherwise, it returns false
     }
 
         //checks for ties 
     function isTie(GD) {
         let isFull = true;
 
-        for (let i = 0; i < GD.length; i++) {
-            isFull = GD[i] && isFull;
-        }
-        if (isFull) {
-            return true;
-        }
+        for (let i = 0; i < GD.length; i++) {isFull = GD[i] && isFull;} //Checks if every single position is full
+        if (isFull) return true;
         return false;
     }
 
     function showGO(player) {
         let Src = `Images/${player}tile.png`;
-        let message = player == "tie" ? "Tie" : "Win";
+        let message = player == "tie" ? "Tie" : "Win"; //If its not a tie, its a win to a player
         GOElement.innerHTML = `
             <h1>${message}</h1>
             <img class="W-img" src=${Src} ></img> 
-            <div class="play" onclick="location.reload();">Reset</div>
-        `;
-    GOElement.classList.remove("hide");
+            <div class="play" onclick="location.reload();">Reset</div> 
+        `;//Creates a win screen
+    GOElement.classList.remove("hide"); //Unhides the screen
     }
 
+    // Adding image sources
+    const Xtile = new Image();
+    Xtile.src = "Images/Xtile150.png";
+    const Otile = new Image();
+    Otile.src = "Images/Otile150.png";
+
+
+    const placement = document.getElementById("place")//sound
+    if (muted == true) placement.volume = 0;
+    
     // deciding and drawing images on position
     function drawEdit(player, i, j) {
         const Image = player == "X" ? Xtile : Otile;
         ctx.drawImage(Image, j * SI, i * SI);
+        
+        placement.play()
     }
 }
--- Javascript/settings.js
+++ Javascript/settings.js
@@ -1,60 +1,86 @@
-const options = document.querySelector(".options");
+const options = document.querySelector(".options"); //Gets all of the classes of html items
 const GOElement = document.querySelector(".gameover");
-
 const CBtn = options.querySelector(".computer");
 const FBtn = options.querySelector(".player");
 const XBtn = options.querySelector(".x");
 const OBtn = options.querySelector(".o");
 const PBtn = options.querySelector(".play");
+let Op; //Defines the opponent
+const player = new Object; // Makes the player object, with the properties computer, friend, and human
+let muted
+//Sounds
 
-let Op;
-const player = new Object;
+
+let select = document.getElementById("select");
+let start = document.getElementById("start")
+let error = document.getElementById("error")
+
+const mute = document.querySelector(".mute");
+const target = document.querySelector(".fas")
+mute.addEventListener("click",function(){
+    if(target.classList.contains("fa-volume-up")){
+        select.volume = 0
+        start.volume = 0
+        error.volume = 0
+        muted = true
+
+        target.classList.remove("fa-volume-up")
+        target.classList.add("fa-volume-mute")
+    }else{
+        select.volume = 1
+        start.volume = 1
+        error.volume = 1
+        muted = false
+
+        target.classList.remove("fa-volume-mute")
+        target.classList.add("fa-volume-up")
+        
+    }
+})
 
 
 
 XBtn.addEventListener("click", function(){
-    player.human = "X";
-    player.computer = "O";
-    player.friend = "O";
-    toggle(OBtn, XBtn);
+    player.human = "X"; player.computer = "O"; player.friend = "O"; //Sets the markers for each player
+    toggle(OBtn, XBtn); //Changes the button colours
+    select.play();
 });
 OBtn.addEventListener("click", function(){
-    player.human = "O";
-    player.computer = "X";
-    player.friend = "X";
+    player.human = "O"; player.computer = "X"; player.friend = "X";
     toggle(XBtn, OBtn);
+    select.play();
 });
 CBtn.addEventListener("click", function(){
-    Op = "computer";
+    Op = "computer";    //Sets the opponent to ai
     toggle(FBtn, CBtn);
-
+    select.play();
 });
 FBtn.addEventListener("click", function(){
-    Op = "friend";
+    Op = "friend"; //Sets the oppenent to another player
     toggle(CBtn, FBtn);
-
+    select.play();
 });
 
 PBtn.addEventListener("click", function(){
     if(!Op){
-        CBtn.style.backgroundColor = "#41444b";
-        FBtn.style.backgroundColor = "#41444b";
-        CBtn.style.color = "#212529";
-        FBtn.style.color = "#212529";
+        CBtn.style.backgroundColor = "#41444b"; FBtn.style.backgroundColor = "#41444b"; //Highlights if no option selected
+        CBtn.style.color = "#212529"; FBtn.style.color = "#212529";
+        error.play()
         return;
     }
     if(!player.human){
-        XBtn.style.backgroundColor = "#41444b";
-        OBtn.style.backgroundColor = "#41444b";
-        XBtn.style.color = "#212529";
-        OBtn.style.color = "#212529";
+        XBtn.style.backgroundColor = "#41444b"; OBtn.style.backgroundColor = "#41444b"; //Highlights if no option selected
+        XBtn.style.color = "#212529"; OBtn.style.color = "#212529";
+        error.play()
         return;
     }
-    init(player, Op);
-    options.classList.add("hide");
+    start.play();
+    init(player, Op, muted);
+    options.classList.add("hide"); //Initiates game and hide the settings
 });
 function toggle(off, on){
-    off.classList.remove("on");
-    on.classList.add("on");
+    off.classList.remove("on"); //Makes the button colour change
+    on.classList.add("on"); //Stops both being active at once
 }
+
 
